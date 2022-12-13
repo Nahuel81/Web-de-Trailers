823d33be6d86d206b490b623c80d18630f9aedd6
Nahuel81 <diegovillan81@gmail.com>
2022-12-13 01:14:43 -0300

    Se corrigio el Error de que no cargue la imagen de Portada, se finalizo la seccion de estrenos. Se deja el proyecto listo para seguir con la seccion de Peliculas

--- src/main/java/com/pelicula/trailers/TrailersApplication.java
+++ src/main/java/com/pelicula/trailers/TrailersApplication.java
@@ -2,6 +2,8 @@ package com.pelicula.trailers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.dialect.springdata.SpringDataDialect;
 
 @SpringBootApplication
 public class TrailersApplication {
@@ -10,4 +12,8 @@ public class TrailersApplication {
 		SpringApplication.run(TrailersApplication.class, args);
 	}
 
+        @Bean
+        public SpringDataDialect springDataDialect(){
+        return new SpringDataDialect();
+        }
 }
---
+++ src/main/java/com/pelicula/trailers/controllers/AssetsController.java
@@ -0,0 +1,23 @@
+
+package com.pelicula.trailers.controllers;
+
+import com.pelicula.trailers.service.AlmacenServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/assets")
+public class AssetsController {
+    @Autowired
+	private AlmacenServiceImpl servicio;
+	
+	@GetMapping("/{filename:.+}")
+	public Resource obtenerRecurso(@PathVariable("filename") String filename) {
+		return servicio.cargarComoRecurso(filename);
+	}
+}
